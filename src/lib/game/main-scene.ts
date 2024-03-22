0da62cf3e40c9042d447a086c05ff396e4ec3da3
Jostein Skaar <jostein.skaar@gmail.com>
2024-03-22 13:02:31 +0100

    feat: heart particles instead of star

--- src/lib/game/main-scene.ts
+++ src/lib/game/main-scene.ts
@@ -210,11 +210,11 @@ export class MainScene extends Phaser.Scene {
 		);
 
 		this.emitter = this.add.particles(0, 0, 'sprites', {
-			frame: 'particle-star-001.png',
+			frame: 'particle-heart-001.png',
 			scale: { start: 1.5, end: 0.5 },
 			speed: { min: 10, max: 100 },
 			lifespan: 800,
-			quantity: 20,
+			quantity: 11,
 			active: false
 		});
 	}
